7d07ae0520458e0408571659afb2874aba75827c
sebascg17 <sebas-clavi@hotmail.com>
2025-05-27 21:00:45 -0500

    Mi primer commit en este proyecto de prueba

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 1000f;
+    private float zBound = 10;
+    private bool onGround = true; // Comienza en el suelo
+    private Rigidbody playerRb;
+
+    void Start ()
+    {
+        playerRb = GetComponent<Rigidbody>(); 
+    }
+
+    void Update ()
+    {
+        MovePlayer();
+        JumpPlayer();
+        ConstrainPlayerPosition();
+    }
+
+    void MovePlayer ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(horizontalInput, 0, verticalInput);
+        //transform.Translate(movimiento * moveSpeed * Time.deltaTime);
+
+        playerRb.AddForce(movimiento * moveSpeed, ForceMode.Force);
+        //playerRb.AddForce(Vector3.forward * moveSpeed * verticalInput);
+        //playerRb.AddForce(Vector3.right * moveSpeed * horizontalInput);
+    }
+
+    void JumpPlayer ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        //onGround = false; // Suponemos que deja el suelo al saltar
+    }
+
+    void ConstrainPlayerPosition ()
+    {
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        else if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+    }
+
+    private void OnCollisionEnter ( Collision collision )
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit ( Collision collision )
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public Transform player; // Asigna el jugador en el Inspector
+    private float fixedY;
+
+    void Start ()
+    {
+        if (player == null)
+        {
+            GameObject playerObj = GameObject.Find("Player");
+            if (playerObj != null)
+                player = playerObj.transform;
+            else
+                Debug.LogError("Player not found.");
+        }
+
+        fixedY = transform.position.y; // Mantener altura constante
+    }
+
+    void LateUpdate ()
+    {
+        if (player == null) return;
+
+        Vector3 newPosition = new Vector3(player.position.x, fixedY, player.position.z + 5);
+        transform.position = newPosition;
+    }
+}
+
